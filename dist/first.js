e0cf3f02edeec9f52c4a2e7e420455e6adba0534
menta <jimementa@gmail.com>
2021-11-30 19:32:12 +0100

    this is typescript

--- dist/first.js
+++ dist/first.js
@@ -1,5 +1,37 @@
 "use strict";
-fetch('https://icanhazdadjoke.com/')
-    .then((res) => res.json())
-    .then((data) => console.log(data));
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+const jokeEl = document.getElementById('joke');
+const get_joke = document.getElementById('get_joke'); // ! es que no va a ser null
+get_joke.addEventListener('click', tellAJoke);
+let joke;
+function tellAJoke() {
+    return __awaiter(this, void 0, void 0, function* () {
+        //llamada a la API
+        const jokeResult = yield fetch('https://icanhazdadjoke.com/', {
+            headers: {
+                'Accept': 'application/json'
+            }
+        });
+        joke = yield jokeResult.json();
+        console.log(joke);
+        jokeEl.innerHTML = joke.joke;
+    });
+}
+function scoreJoke(joke, resultado) {
+    const jokeReport = {
+        joke,
+        resultado,
+        date: new Date().toISOString()
+    };
+    jokesReports.push(jokeReport);
+}
+const jokesReports = [];
 //# sourceMappingURL=first.js.map
--- first.ts
+++ first.ts
@@ -1,4 +1,40 @@
 
-fetch ('https://icanhazdadjoke.com/')
-.then((res) => res.json())
-.then ((data) => console.log(data))
+const jokeEl : HTMLElement = document.getElementById('joke')!;
+const get_joke : HTMLElement = document.getElementById('get_joke')! // ! es que no va a ser null
+
+get_joke.addEventListener('click',tellAJoke)
+let joke : Joke 
+
+async function tellAJoke(){
+    //llamada a la API
+    const jokeResult = await fetch('https://icanhazdadjoke.com/',{
+        headers:{
+            'Accept': 'application/json'
+        }
+    })
+
+    joke = await jokeResult.json();
+    console.log(joke);
+    jokeEl.innerHTML = joke.joke
+}
+
+interface Joke{
+    id : string;
+    joke : string;
+}
+interface JokesReports {
+    joke : Joke;
+    resultado : number;
+    date : string;
+} 
+
+function scoreJoke(joke : Joke, resultado : number){
+    const jokeReport: JokesReports = {
+        joke, 
+        resultado, 
+        date : new Date().toISOString()
+    }
+    jokesReports.push(jokeReport)
+}
+
+const jokesReports: JokesReports[] = [] 
